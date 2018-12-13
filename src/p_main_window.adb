94aaf0abb79ee883a955984d6c49da1712f405de
YruamaLairba <Yruama_Lairba@hotmail.com>
2018-12-13 22:38:45 +0100

    Create a 10x10 grid of Cell

--- src/p_main_window.adb
+++ src/p_main_window.adb
@@ -9,37 +9,44 @@ package body P_Main_Window is
 
    procedure Initialize(Main_Window : in out T_Main_Window) is
    begin
+      Main_Window.Game.Height := 10;
+      Main_Window.Game.Width := 10;
+      Main_Window.Game.Nb_Mine := 10;
+
       Gtk_New(GTK_Window(Main_Window.Win),Window_Toplevel) ;
       Main_Window.Win.Set_Title("Demineur");
-      Gtk_New_Hbox(Main_Window.Box);
+      Gtk_New(
+         Main_Window.Table,
+         Guint(Main_Window.Game.Height),
+         Guint(Main_Window.Game.Width),
+         true);
 
-      Initialize(Main_Window.Cell1);
-      Initialize(Main_Window.Cell2);
-      
-      Main_Window.Cell1.Mined := true;
-      Main_Window.Cell2.Nb_Foreign_Mine := 5;
-      
-      Main_Window.Box.Pack_Start(
-         Main_Window.Cell1.Button,
-         Expand=>true);
-      Main_Window.Box.Pack_Start(
-         Main_Window.Cell2.Button,
-         Expand=>true);
-      Main_Window.Win.Add(Main_Window.Box);
+      Main_Window.Cells := new T_Cell_Tab(
+         1..Main_Window.Game.Height,
+         1..Main_Window.Game.Width);
+
+      for row in Main_Window.Cells'Range(1) loop
+         for col in Main_Window.Cells'Range(2) loop
+            Initialize(Main_Window.Cells(row, col));
+            Main_Window.Table.Attach(
+               --Main_Window.Table,
+               Main_Window.Cells(row,col).Button,
+               Guint(col-1),
+               Guint(col),
+               Guint(row-1),
+               Guint(row));
+
+            Connect(
+               Main_Window.Cells(row,col).Button,
+               "button_press_event",
+               To_Marshaller(Cell_Clicked_Callback'access),
+               Main_Window.Cells(row,col));
+         end loop;
+      end loop;
+
+      Main_Window.Win.Add(Main_Window.Table);
 
       Connect(Main_Window.Win, "destroy", Stop_Program'access) ;
-
-      Connect(
-         Main_Window.Cell1.Button,
-         "button_press_event",
-         To_Marshaller(Cell_Clicked_Callback'access),
-         Main_Window.Cell1) ;
-
-      Connect(
-         Main_Window.Cell2.Button,
-         "button_press_event",
-         To_Marshaller(Cell_Clicked_Callback'access),
-         Main_Window.Cell2) ;
 
       Main_Window.Win.Show_All;
    end Initialize;
--- src/p_main_window.ads
+++ src/p_main_window.ads
@@ -1,9 +1,10 @@
 with Ada.Finalization; use Ada.Finalization;
+with Glib; use Glib;
 with Gdk.Event; use Gdk.Event;
 with Gtk.Button; use Gtk.Button;
 with Gtk.Enums; use Gtk.Enums;
 with Gtk.Handlers ;
-with Gtk.Box; use Gtk.box;
+with Gtk.Table; use Gtk.Table;
 with Gtk.HButton_Box ;   use Gtk.HButton_Box ;
 with Gtk.Main; use Gtk.Main;
 with Gtk.Widget; use Gtk.Widget;
@@ -12,11 +13,19 @@ with P_Cell; use P_Cell;
 
 
 package P_Main_Window is
+   type T_Game is record
+      Height: natural;
+      Width: natural;
+      Nb_Mine: natural;
+   end record;
+
+   type T_Cell_Tab is array (natural range<>,natural range<>) of T_Cell;
+
    type T_Main_Window is new Controlled with record
       Win: Gtk_Window;
-      Box: Gtk_Hbox;
-      Cell1: T_Cell;
-      Cell2: T_Cell;
+      Table: Gtk_Table;
+      Cells : access T_Cell_Tab;
+      Game: T_Game;
    end record;
 
    procedure Stop_Program(Emetteur : access Gtk_Widget_Record'class);
